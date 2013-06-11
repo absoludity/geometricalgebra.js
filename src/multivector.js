cccacd3583777125151650cb0fe6136e38f08d2f
Michael Nelson <absoludity@gmail.com>
2013-06-11 20:37:33 +0200

    Simplified multivector aggregation.

--- src/multivector.js
+++ src/multivector.js
@@ -3,25 +3,19 @@ define(['underscore', 'multivectorterm'], function(_, mvt) {
 
 var MultiVector = function(terms) {
 
-    var factors = {};
-    _.each(terms, function(term){
-        var basis, factor;
-        if (_.isNumber(term)) {
-            basis = '';
-            factor = term;
-        } else {
-            basis = term.basis.toString();
-            factor = term.factor;
-        }
-        factors[basis] = _.has(factors, basis) ? factors[basis] + factor : factor;
+    // Handle numbers being passed as terms.
+    terms = _.map(terms, function(term) {
+        return _.isNumber(term) ? new mvt.MultiVectorTerm(term, []) : term;
     });
-    this.terms = _.map(_.pairs(factors), function(basis_factor) {
-        var basis = basis_factor[0];
-        basis = basis ? basis.split(',') : [];
-        basis = _.map(basis, function(base) {
-            return parseInt(base, 10);
-        });
-        return new mvt.MultiVectorTerm(basis_factor[1], basis);
+
+    // Group the terms by their basis, then aggregate the factors for each
+    // basis, returning a single term per basis.
+    var terms_by_basis = _.groupBy(terms, 'basis');
+    this.terms = _.map(_.values(terms_by_basis), function(terms_for_basis) {
+        var factor = _.reduce(terms_for_basis, function(memo, term) {
+            return memo + term.factor;
+        }, 0);
+        return new mvt.MultiVectorTerm(factor, terms_for_basis[0].basis);
     });
 };
 
